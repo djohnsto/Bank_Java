ad92a0ac6c45b3b73c97a87518e596eccd2bb707
Kyle Rader <krader@thoughtworks.com>
2010-03-15 16:22:39 -0500

    Sample test

---
+++ Web_App_Tests/Bank_Account_Tests.java
@@ -0,0 +1,10 @@
+package Web_App_Tests;
+import org.junit.Assert;
+import org.junit.Test;
+public class Bank_Account_Tests {
+  @Test
+  public void booleanSample()
+  {
+    Assert.assertTrue(true);
+  }
+}
